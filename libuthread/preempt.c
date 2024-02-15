1ab047b037d31efc4c0f08850ad0e5c097b847db
Gur10go <156872507+Gur10go@users.noreply.github.com>
2024-02-14 17:14:29 -0800

    added preemption

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -14,24 +14,55 @@
  * 100Hz is 100 times per second
  */
 #define HZ 100
+#define secCnvrtMicro 1000000
+#define wholeSec 0
+struct sigaction saDef;
+struct itimerval old; 
+void alrm_handler(int signum){
+	uthread_yield();
+}
 
 void preempt_disable(void)
 {
-	/* TODO Phase 4 */
+    sigemptyset(&saDef);
+    sigaddset(&saDef, SIGVTALRM);
+    sigprocmask(SIG_BLOCK, &saDef, NULL);
 }
 
 void preempt_enable(void)
 {
-	/* TODO Phase 4 */
+    sigemptyset(&saDef);
+    sigaddset(&saDef, SIGVTALRM);
+    sigprocmask(SIG_UNBLOCK, &saDef, NULL);
 }
 
 void preempt_start(bool preempt)
 {
-	/* TODO Phase 4 */
+
+	if (!preempt) {
+		return;
+	}
+	saDef.sa_handler = alrm_handler;
+	sigemptyset(&saDef.sa_mask);
+	saDef.sa_flags = 0;
+	sigaction(SIGVTALRM, &saDef, NULL);
+
+	//make in such that it runs 100 times per second
+	struct itimerval timer;
+	timer.it_value.tv_sec = wholeSec;
+	timer.it_value.tv_usec = secCnvrtMicro/HZ;
+
+	//put in a initial start to 100microsec/100
+	timer.it_interval.tv_sec = wholeSec;
+	timer.it_interval.tv_usec = secCnvrtMicro/HZ;
+
+	//initalize new Itimerval for initial signal time and for everyother signal after that
+	setitimer(ITIMER_VIRTUAL, &timer, &old);
 }
 
 void preempt_stop(void)
 {
-	/* TODO Phase 4 */
+	setitimer(ITIMER_VIRTUAL, &old, NULL);
+	sigaction(SIGVTALRM, &saDef, NULL);
 }
 

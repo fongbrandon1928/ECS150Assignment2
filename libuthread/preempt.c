2e6c89c4d036bf022c98c0aa5761f5afdbf1e435
fongbrandon1928 <146702122+fongbrandon1928@users.noreply.github.com>
2024-02-15 17:53:26 -0800

    Update preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -17,32 +17,34 @@
 #define secCnvrtMicro 1000000
 #define wholeSec 0
 struct sigaction saDef;
-struct itimerval old; 
+struct itimerval old;
 sigset_t sigset;
 
-void alrm_handler(int signum){
+void alrm_handler(int signum)
+{
 	(void)signum;
 	uthread_yield();
 }
 
 void preempt_disable(void)
 {
-    sigemptyset(&sigset);
-    sigaddset(&sigset, SIGVTALRM);
-    sigprocmask(SIG_BLOCK, &sigset, NULL);
+	sigemptyset(&sigset);
+	sigaddset(&sigset, SIGVTALRM);
+	sigprocmask(SIG_BLOCK, &sigset, NULL);
 }
 
 void preempt_enable(void)
 {
-    sigemptyset(&sigset);
-    sigaddset(&sigset, SIGVTALRM);
-    sigprocmask(SIG_UNBLOCK, &sigset, NULL);
+	sigemptyset(&sigset);
+	sigaddset(&sigset, SIGVTALRM);
+	sigprocmask(SIG_UNBLOCK, &sigset, NULL);
 }
 
 void preempt_start(bool preempt)
 {
 	printf("preempt started1");
-	if (!preempt) {
+	if (!preempt)
+	{
 		return;
 	}
 	printf("preempt started2");
@@ -51,16 +53,16 @@ void preempt_start(bool preempt)
 	saDef.sa_flags = 0;
 	sigaction(SIGVTALRM, &saDef, NULL);
 	printf("preempt started3");
-	//make in such that it runs 100 times per second
+	// make in such that it runs 100 times per second
 	struct itimerval timer;
 	timer.it_value.tv_sec = wholeSec;
-	timer.it_value.tv_usec = secCnvrtMicro/HZ;
+	timer.it_value.tv_usec = secCnvrtMicro / HZ;
 	printf("preempt started4");
-	//put in a initial start to 100microsec/100
+	// put in a initial start to 100microsec/100
 	timer.it_interval.tv_sec = wholeSec;
-	timer.it_interval.tv_usec = secCnvrtMicro/HZ;
+	timer.it_interval.tv_usec = secCnvrtMicro / HZ;
 	printf("preempt started5");
-	//initalize new Itimerval for initial signal time and for everyother signal after that
+	// initalize new Itimerval for initial signal time and for everyother signal after that
 	setitimer(ITIMER_VIRTUAL, &timer, &old);
 	printf("preempt started6");
 }

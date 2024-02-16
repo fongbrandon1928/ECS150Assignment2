954edc62dea8ea1c65871825fde642c1e27d4a95
fongbrandon1928 <146702122+fongbrandon1928@users.noreply.github.com>
2024-02-15 23:50:29 -0800

    Update preempt_test.c

--- apps/preempt_test.c
+++ apps/preempt_test.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 
 #include <uthread.h>
+#include "private.h"
 
 unsigned long fib(unsigned int n) {
     if (n == 0) return 0;
@@ -11,37 +12,55 @@ unsigned long fib(unsigned int n) {
     return fib(n - 1) + fib(n - 2);
 }
 
+void thread4(void *arg)
+{
+    (void)arg;
+    for (int i = 0; i <= 40; i++) {
+    	if (i == 35) {
+    		preempt_stop();
+    		printf("Stopped Preemption\n");
+    	}
+        printf("thread 4(%d): %ld\n", i, fib(i));
+    }
+    printf("thread4 complete\n");
+}
+
 void thread3(void *arg)
 {
     (void)arg;
-    for (int i = 0; i < 10000000; i++) {
-        printf("thread3: %d\n", i);
+    uthread_create(thread4, NULL);
+    for (int i = 0; i <= 40; i++) {
+        printf("thread 3(%d): %ld\n", i, fib(i));
     }
-    printf("thread3\n");
+    printf("thread3 complete.\n");
 }
 
 void thread2(void *arg)
 {
     (void)arg;
     uthread_create(thread3, NULL);
-    for (int i = 0; i < 10000000; i++) {
-        printf("thread2: %d\n", i);
+    for (int i = 0; i <= 40; i++) {
+        printf("thread 2(%d): %ld\n", i, fib(i));
     }
-    printf("thread2\n");
+    printf("thread2 complete.\n");
 }
 
 void thread1(void *arg)
 {
     (void)arg;
     uthread_create(thread2, NULL);
-    for (int i = 0; i < 10000000; i++) {
-    	printf("thread1: %d\n", i);
+    for (int i = 0; i <= 40; i++) {
+    	if (i == 35) {
+    		preempt_start(true);
+    		printf("Started Preemption\n");
+    	}
+    	printf("thread 1(%d): %ld\n", i, fib(i));
     }
-    printf("thread1\n");
+    printf("thread1 complete.\n");
 }
 
 int main(void)
 {
-    uthread_run(true, thread1, NULL);
+    uthread_run(false, thread1, NULL);
     return 0;
 }

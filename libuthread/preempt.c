dff70e8c2a6514255b495f24a865660ea1375819
fongbrandon1928 <146702122+fongbrandon1928@users.noreply.github.com>
2024-02-15 23:50:51 -0800

    Update preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -42,29 +42,28 @@ void preempt_enable(void)
 
 void preempt_start(bool preempt)
 {
-	printf("preempt started1");
 	if (!preempt)
 	{
 		return;
 	}
-	printf("preempt started2");
+
 	saDef.sa_handler = alrm_handler;
 	sigemptyset(&saDef.sa_mask);
 	saDef.sa_flags = 0;
 	sigaction(SIGVTALRM, &saDef, NULL);
-	printf("preempt started3");
+
 	// make in such that it runs 100 times per second
 	struct itimerval timer;
 	timer.it_value.tv_sec = wholeSec;
 	timer.it_value.tv_usec = secCnvrtMicro / HZ;
-	printf("preempt started4");
+
 	// put in a initial start to 100microsec/100
 	timer.it_interval.tv_sec = wholeSec;
 	timer.it_interval.tv_usec = secCnvrtMicro / HZ;
-	printf("preempt started5");
+
 	// initalize new Itimerval for initial signal time and for everyother signal after that
 	setitimer(ITIMER_VIRTUAL, &timer, &old);
-	printf("preempt started6");
+
 }
 
 void preempt_stop(void)
